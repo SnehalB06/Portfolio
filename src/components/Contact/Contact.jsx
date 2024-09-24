e681d03a7eddee862ba435e5ba24ac4e0fbaf8d3
Bsnehal06 <snehalbhole061297@gmail.com>
2024-09-24 01:00:52 -0400

    SkilChnages

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -26,6 +26,15 @@ export const Contact = () => {
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
           <a href="https://github.com/SnehalB06">github.com/snehal-bhole</a>
         </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/hack-re.png")} alt="Hackerrank Logo" />
+          <a href="https://www.hackerrank.com/profile/snehalbhole06121">Hackerrank.com/snehal-bhole</a>
+        </li>
+        
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/leet-re.png")} alt="Github icon" />
+          <a href="https://leetcode.com/u/Bsnehal/">leetcode.com/snehal-bhole</a>
+        </li>
       </ul>
     </footer>
   );
